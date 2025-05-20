7559c30914f4f0078cc71cca87d257ebfead96ab
Arman <khanarmanahmad0@gmail.com>
2025-05-20 13:13:21 +0530

    Added Testimonials section and added smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Header></Header>
       <About/>
       <Projects/>
+      <Testimonials/>
 
     </div>
     
